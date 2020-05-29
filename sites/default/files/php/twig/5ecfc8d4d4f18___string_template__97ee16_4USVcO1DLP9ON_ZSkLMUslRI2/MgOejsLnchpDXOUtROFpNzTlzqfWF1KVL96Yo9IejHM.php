7f15042a80aaed1a04b121b831bcc4419f16cf47
jpfrancesconi <juan.francesconi@devlights.com>
2020-05-29 10:40:15 -0300

    improving the login page

---
+++ sites/default/files/php/twig/5ecfc8d4d4f18___string_template__97ee16_4USVcO1DLP9ON_ZSkLMUslRI2/MgOejsLnchpDXOUtROFpNzTlzqfWF1KVL96Yo9IejHM.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__97ee16d532741043229fd38f63ff95af645b37f9aed9da16b927aedcb787d58d */
+class __TwigTemplate_b70fdb68553001f765c073b6eb40957601d48a0fc862f7a45bfde0a0d74ebe58 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "http://localhost/ideasorange//sites/default/files/logo.png";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__97ee16d532741043229fd38f63ff95af645b37f9aed9da16b927aedcb787d58d";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__97ee16d532741043229fd38f63ff95af645b37f9aed9da16b927aedcb787d58d", "");
+    }
+}
